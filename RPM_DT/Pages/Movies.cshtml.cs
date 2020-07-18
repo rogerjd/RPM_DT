370aa314e99395499a354603fc50a3d11fbc8b84
rjpd@netzero.com <rogerjd>
2020-07-18 18:22:21 -0400

    added Movies page, wip

---
+++ RPM_DT/Pages/Movies.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RPM_DT.Pages
+{
+    public class MoviesModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
