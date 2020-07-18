c8898a47a7990cca856dd37a4470533afe1ed86f
rjpd@netzero.com <rogerjd>
2020-07-18 18:36:11 -0400

    Movies pages, added get table

--- RPM_DT/Pages/Index.cshtml.cs
+++ RPM_DT/Pages/Index.cshtml.cs
@@ -6,36 +6,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Configuration;
-using System.Data.SqlClient;
-using System.Data;
+
 
 namespace RPM_DT.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        private readonly IConfiguration _configuration;
 
-        public DataTable Movie { get; set; }
-
-        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
-            _configuration = configuration;
         }
 
         public void OnGet()
         {
-            string ConnStr = _configuration.GetConnectionString("RazorPagesMovieContext");
-            SqlConnection conn = new SqlConnection(ConnStr);
-            conn.Open();
-            SqlCommand command = new SqlCommand("Select * from Movie", conn);
-            SqlDataAdapter da = new SqlDataAdapter(command);
-            Movie = new DataTable();
-            da.Fill(Movie);
-
-            conn.Close();
         }
     }
 }
--- RPM_DT/Pages/Movies.cshtml.cs
+++ RPM_DT/Pages/Movies.cshtml.cs
@@ -4,14 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace RPM_DT.Pages
 {
     public class MoviesModel : PageModel
     {
+        private readonly IConfiguration _configuration;
+
+        public DataTable Movie { get; set; }
+
+        public MoviesModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void OnGet()
         {
+            string ConnStr = _configuration.GetConnectionString("RazorPagesMovieContext");
+            SqlConnection conn = new SqlConnection(ConnStr);
+            conn.Open();
+            SqlCommand command = new SqlCommand("Select * from Movie", conn);
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            Movie = new DataTable();
+            da.Fill(Movie);
 
+            conn.Close();
         }
     }
 }
