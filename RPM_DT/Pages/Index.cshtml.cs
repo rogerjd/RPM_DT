b5c0aa587f35ffd5fd57bebcca1f7c2a4fc5388d
rjpd@netzero.com <rogerjd>
2020-07-18 16:11:21 -0400

    GetConnectionString, ok

--- RPM_DT/Pages/Index.cshtml.cs
+++ RPM_DT/Pages/Index.cshtml.cs
@@ -1,25 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace RPM_DT.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
-
+            string connStr = _configuration.GetConnectionString("RazorPagesMovieContext");
         }
     }
 }
