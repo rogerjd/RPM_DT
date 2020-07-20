5afe1145af15563374459e580cf66d298d467a42
rjpd@netzero.com <rogerjd>
2020-07-20 12:42:54 -0400

    Building Create form, wip

---
+++ RPM_DT/Pages/Movie/Create.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace RPM_DT.Pages.Movie
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync([Required] string Title, string Genre, decimal Price, DateTime ReleaseDate, string Rating)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToPage("/Movie/Index");
+            }
+            return Page();
+        }
+    }
+}
