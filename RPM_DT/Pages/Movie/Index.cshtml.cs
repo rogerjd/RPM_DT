3d60b4f74bf5c42382113e02cd17d777728526af
rjpd@netzero.com <rogerjd>
2020-07-23 15:56:20 -0400

    Format Datetime

--- RPM_DT/Pages/Movie/Index.cshtml.cs
+++ RPM_DT/Pages/Movie/Index.cshtml.cs
@@ -25,5 +25,17 @@ namespace RPM_DT.Pages
         {
             Movie = DB.Database.GetMovies(_configuration);
         }
+
+        string getDate(object obj)
+        {
+            if (obj is DateTime dt)
+            {
+                return dt.ToShortDateString();
+            }
+            else
+            {
+                return "";
+            }
+        }
     }
 }
